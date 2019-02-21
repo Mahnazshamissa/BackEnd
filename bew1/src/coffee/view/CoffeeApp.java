7794301a102aae9fa3f4b55feebb6b40829b0324
Mahnazshamissa <f.bakhshiani@gmail.com>
2019-02-21 12:14:38 +0100

    refactor,FileAppender

--- bew1/src/coffee/view/CoffeeApp.java
+++ bew1/src/coffee/view/CoffeeApp.java
@@ -7,7 +7,7 @@ public class CoffeeApp {
     public static void main(String[] args) {
 
         BeanStatisticsGatherer beanStatisticsGatherer = new BeanStatisticsGatherer();
-        beanStatisticsGatherer.gatherStatistics(17481);
+        beanStatisticsGatherer.gatherStatistics(10001);
 
         //the outcomes are suspiciously close to 50 percent
     }
