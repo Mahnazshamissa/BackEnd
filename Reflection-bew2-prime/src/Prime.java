4bdb6293503b176bf50be73460b276501d96906f
Mahnazshamissa <f.bakhshiani@gmail.com>
2019-03-01 11:38:34 +0100

    Prime-Java11

--- Reflection-bew2-prime/src/Prime.java
+++ Reflection-bew2-prime/src/Prime.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.*;
 
 public class Prime {
+
+    public List<Integer> getPrimeTill100() {
+        return IntStream.range(1, 100)
+                .boxed()
+                .filter(this::hasNoFactors)
+                .collect(toList());
+
+    }
+    private boolean hasNoFactors(int number) {
+        return IntStream.range(2, number)
+                .noneMatch(e -> number % e == 0);
+    }
+
 }
+/*
+
+
+List<Integer> primes = new ArrayList<>();
+        IntStream.range(1, 100)
+                .filter(n -> primes.parallelStream()
+                .nonMatch(p -> n % p == 0))
+                .forEach(primes::add);
+ */
